3e636a2707b5b5d9a78b340d2049f489581bfd30
Utkarsh Tiwari <utkarshthor12@gmail.com>
2024-12-21 07:58:26 +0530

    created Grid Section

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
     return (
-        <div>Grid</div>
+        <section id="about">
+        <BentoGrid className="w-full py-20">
+            {gridItems.map((item, i) => (
+            <BentoGridItem
+                id={item.id}
+                key={i}
+                title={item.title}
+                description={item.description}
+                // remove icon prop
+                // remove original classname condition
+                className={item.className}
+                img={item.img}
+                imgClassName={item.imgClassName}
+                titleClassName={item.titleClassName}
+                spareImg={item.spareImg}
+            />
+            ))}
+        </BentoGrid>
+    </section>
     )
 }
 
