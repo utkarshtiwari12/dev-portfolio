24da2a3c92afdbfc0d7c477304376c4fde2ea93b
Utkarsh Tiwari <utkarshthor12@gmail.com>
2024-12-23 11:45:48 +0530

    errors during production build

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
 
 export default withSentryConfig(nextConfig, {
